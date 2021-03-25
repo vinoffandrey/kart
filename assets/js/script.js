3cfb804a38f251dc2cccd5757f29abfe3d79d122
vinoffandrey <69467871+vinoffandrey@users.noreply.github.com>
2021-03-25 20:42:12 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+	$('.slider__body').slick({
+		slidesToShow: 3,
+		sliderToScroll: 1,
+		speed: 500,
+		easing: 'ease',
+		waitForAnimate: false,
+		infinite: false,
+		responsive: [
+			{
+				breakpoint: 769,
+				settings: {
+					slidesToShow: 2,
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 1,
+				}
+			},
+			{
+				breakpoint: 450,
+				settings: {
+					slidesToShow: 1
+				}
+			}
+		]
+	});
+});
+
+const cards = document.querySelectorAll('.slider__item');
+
+function flipCard() {
+	this.classList.toggle('flip');
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
+
+
+
+$(".open__button").click(function () {
+	$(".slider__container").toggleClass('active');
+});
